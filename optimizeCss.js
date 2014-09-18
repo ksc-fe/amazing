082a88acd48e1cd102f1a058f1e9ec9ac993b72e
kreding <juxuejian1@163.com>
2014-09-18 17:57:27 +0800

    添加图片处理

--- optimizeCss.js
+++ optimizeCss.js
@@ -3,33 +3,85 @@
  */
 var fs = require('fs');
 var path = require('path');
-var md5 = require('./tools').md5;
 
+var spriter = require('ispriter');
 var compressor = require('yuicompressor');
+var ndir = require('ndir');
+var md5 = require('./tools').md5;
+// var spriteConfig = require('./sprite-config.json');
+
+var spriteConfig = 
+{
+    "workspace": "../console/fe-source",
+ 
+    "input": {
+ 
+        "cssSource": ["./resources/temp/**.css"],
+ 
+        // "ignoreImages": ["*logo.png"],
+ 
+        "format": "png"
+    },
+
+    "output": {
+ 
+        "cssDist": "./pkg/css/",
+ 
+        "imageDist": "../../resources/images/sprite/",
+ 
+        "maxSingleSize": 300,
+ 
+        "margin": 5,
+ 
+        "prefix": "sprite_",
+ 
+        "format": "png",
+ 
+        "combine": true,
+
+        "splitCss": false,
+ 
+        "combineCSSRule": false,
+ 
+        "compress": true
+    }
+}
 
 var cssReg = /\s*#set\s*\(\s*\$cssList\s*=\s*\[\s*"\s*([\/]?[.\w-]+)([\/][.\w-]+)*\s*"\s*(,\s*"\s*([\/]?[.\w-]+)([\/][.\w-]+)*\s*"\s*)*\s*\]\s*\)\s*/i;
-var basePath = path.resolve('../console/fe-source/pkg') + '/';
+var basePath = path.resolve('../console/fe-source/resources') + '/';
 var cssArr = [];
 
-var packageCss = function( allCss, file ) {
-    var md5Part = md5( allCss.toString() ).slice(0, 10) 
-    var output = basePath + md5Part + '.min.css';
+var packageCss = function( cssArr, file ) {
+    var md5Part = md5( cssArr.toString() ).slice(0, 10) 
+    var output = basePath + 'temp/' + md5Part + '.min.css';
     console.log('version number: ', md5Part);
+
+    fs.exists(basePath + 'temp/', function (exists) {
+        console.log('*******************exists: ', exists)
+        if( !exists ) {
+            ndir.mkdir(basePath + 'temp/');
+        }
+    });
 
     fs.readFile(file, function( err, data ){
         if(err) throw err;
-        fs.writeFile(output, allCss, function(err){
+        fs.writeFile(output, cssArr, function(err){
             if(err) throw err;
             console.log('Write ' + output + ' success!')
+            spriteConfig.output.combine = md5Part + '.min.css'
 
-            var originCont = data.toString();
-            var updateCont = originCont.replace(cssReg, '\r\n#set($cssList = ["' + output + '"])\r\n');
+            spriter.merge(spriteConfig, function(){
+                var _basePath = path.resolve('../console/fe-source/pkg/css') + '/';
+                var _output = _basePath + md5Part + '.min.css';
+                var originCont = data.toString();
+                var updateCont = originCont.replace(cssReg, '\r\n#set($cssList = ["' + _output + '"])\r\n');
 
-            var fileBuf = new Buffer(updateCont);
-            fs.writeFile(file, fileBuf, function(err){
-                if(err) throw err;
-                console.log('Write ' + file + ' success')
-            })
+                var fileBuf = new Buffer(updateCont);
+                fs.writeFile(file, fileBuf, function(err){
+                    if(err) throw err;
+                    console.log('Write ' + file + ' success')
+                })
+            });
         })
     })
     
